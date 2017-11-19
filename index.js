94981d79aa1dbb99bc417ebc5bcedac7911c5492
Dani Lewandowski <dani@splashthat.com>
2017-11-19 15:29:32 -0500

    Reorganized db and response handling into its own middleware folder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const client = require('./client');
+const client = require('./middleware/client');
 
 const app = express();
 
